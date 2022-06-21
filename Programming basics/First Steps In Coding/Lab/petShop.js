454d2f7797a4e24784d6d9a3414f02d2ac8f760d
CodingNameKiki <107936530+CodingNameKiki@users.noreply.github.com>
2022-06-21 17:07:29 +0300

    Create petShop.js

---
+++ Programming basics/First Steps In Coding/Lab/petShop.js
@@ -0,0 +1,8 @@
+function petShop(input) {
+    let dogFood = Number(input[0]);
+    let catFood = Number(input[1]);
+    let priceDog = 2.50;
+    let priceCat = 4;
+    let result = (dogFood * priceDog) + (catFood * priceCat)
+    console.log(`${result} lv.`)
+}
