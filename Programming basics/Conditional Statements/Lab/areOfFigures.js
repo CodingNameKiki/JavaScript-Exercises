df1c1951f7eb42472e2b5ccb723b7b51bd97c2f6
CodingNameKiki <107936530+CodingNameKiki@users.noreply.github.com>
2022-06-22 20:22:16 +0300

    Update areOfFigures.js

--- Programming basics/Conditional Statements/Lab/areOfFigures.js
+++ Programming basics/Conditional Statements/Lab/areOfFigures.js
@@ -4,20 +4,17 @@ function areaOfFigures (input) {
     if (figure == "square"){
         let a = Number(input[1])
         area = a * a;
-        console.log(area.toFixed(3))
     }else if (figure == "rectangle"){
         let a = Number(input[1]);
         let b = Number(input[2]);
         area = a * b;
-        console.log(area.toFixed(3))
     }else if (figure == "circle"){
         let r = Number(input[1]);
         area = (r * r) * Math.PI;
-        console.log(area.toFixed(3))
     }else if (figure == "triangle") {
         let a = Number(input[1]);
         let h = Number(input[2]);
         area = (a * h) / 2
-        console.log(area.toFixed(3))
     }
+    console.log(area.toFixed(3))
 }
