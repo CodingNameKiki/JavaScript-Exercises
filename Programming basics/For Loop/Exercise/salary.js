32d71bdcb41f1b439dc1e1d40dcebaf9f002f71a
CodingNameKiki <107936530+CodingNameKiki@users.noreply.github.com>
2022-06-25 17:32:07 +0300

    Create salary.js

---
+++ Programming basics/For Loop/Exercise/salary.js
@@ -0,0 +1,22 @@
+function salary (input) {
+    let n = Number(input[0]);
+    let salary = Number(input[1]);
+    for (let i = 0; i <= n; i++){
+        let tabs = String(input[i]);
+        switch(tabs){
+            case "Facebook":
+                salary -= 150;
+            break;
+            case "Instagram":
+                salary -= 100;
+            break;
+            case "Reddit":
+                salary -= 50;
+            break;
+        }
+    }if (salary <= 0){
+        console.log(`You have lost your salary.`)
+    }else {
+        console.log(salary)
+    }
+}
