122188aa723ce7c7b2fa2f8931312738c85d4379
CodingNameKiki <107936530+CodingNameKiki@users.noreply.github.com>
2022-06-28 12:31:16 +0300

    Create sequence2k.js

---
+++ Programming basics/While loop/Lab/sequence2k.js
@@ -0,0 +1,8 @@
+function sequence (input) {
+    let number = Number(input[0]);
+    let count = 1;
+    while (count <= number){
+        console.log(count)
+        count = count * 2 + 1;
+        }
+}
